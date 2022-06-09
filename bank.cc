b26c1455512bdbcdb9e0346742401436aac2b596
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-06-09 08:15:34 +0200

    feat: function declaration

--- bank.cc
+++ bank.cc
@@ -1,17 +1,18 @@
-#include <iostream>
-#include <fstream>
-#include <cctype>
+#include<iostream>
+#include<fstream>
+#include<cctype>
+
 
 using namespace std;
 
-class account
-{
+class account{
     int acno;
     char name[50];
     int deposit;
     char type;
 
-public:
+    public: 
+
     void create_account();
     void show_account();
     void modify();
@@ -21,47 +22,27 @@ public:
     int racno() const;
     int redeposit() const;
     char rettype() const;
-    bool isDepositCorrect(int deposit, char type);
 };
 
-void account::create_account()
-{
-    cout << "Enter account No" << endl;
-    cin >> acno;
-    cout << "Enter the name of account holder" << endl;
-    cin >> name;
-    cout << "Enter the type of account (C(current)/S(saving))" << endl;
-    cin >> type;
+
+void account::create_account(){
+    cout <<"Enter account No"<<endl;
+    cin>>acno;
+    cout<<"Enter the name of account holder"<<endl;
+    cin>>name;
+    cout<<"Enter the type of account (C(current)/S(saving))"<<endl;
+    cin>>type;
     type = toupper(type);
-    cout << "Enter initial amount (>=500 for saving and >=1000 for current)" << endl;
-    cin >> deposit;
-    bool status = isDepositCorrect(deposit,type);
-    
+    cout<<"Enter initial amount (>=500 for saving and >=1000 for current)"<<endl;
+    cin>>deposit;
 }
 
-bool account::isDepositCorrect(int deposit, char type)
-{
-    switch (type)
-    {
-    case 'C':
-        if (deposit <= 1000)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    case 'S':
 
-    if(deposit<=500){
-        return false;
-    }
-    else{
-        return true;
-    }
-
-    default:
-       cout<<"Invalid choice";    
-    }
-}
+void write_account();
+void display_sp(int);
+void modify_account(int);
+void delete_account(int);
+void display_all();
+void deposit_all();
+void deposit_withdraw(int,int);
+void intro();
