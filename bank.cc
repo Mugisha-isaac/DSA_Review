fa6107fa289888177297ab3eda51d0471d924b71
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-06-09 09:30:16 +0200

    feat: displaying function

--- bank.cc
+++ bank.cc
@@ -183,3 +183,24 @@ void delete_account(int n){
     rename("Temp.dat","account.dat");
     cout<<"Record Deleted"<<endl;
 }
+
+
+void display_all(){
+    account ac;
+    ifstream inFile;
+    inFile.open("account.dat",ios::binary);
+    if(!inFile){
+        cout<<"File could not be open !! Press any Key...";
+		return;
+    }
+
+    cout<<"\n\n\t\tACCOUNT HOLDER LIST\n\n";
+	cout<<"====================================================\n";
+	cout<<"A/c no.      NAME           Type  Balance\n";
+	cout<<"====================================================\n";
+    
+    while(inFile.read(reinterpret_cast<char *>(&ac),sizeof(account))){
+        ac.report();
+    }
+    inFile.close()
+}
